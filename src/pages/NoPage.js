9ca847a8eafe2103a1b0f3c5ce4897399f77252b
ChrissQuartz <69059820+thecrazytech1@users.noreply.github.com>
2023-11-30 12:34:11 +1100

    404 page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,12 @@
 const NoPage = () => {
-  return <h1>404</h1>;
+  return (
+    <>
+      <h1>404</h1>
+      <h2>Are you lost?</h2>
+      <h2>It seems the pages you were looking for doesnt exist</h2>
+      <h2>Click <a href="/"><strong>Here</strong></a> to go back to the Homepage</h2>
+    </>
+  );
 };
 
 export default NoPage;
